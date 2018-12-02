35a46aee14993c87470f978a1bcc284f03874d74
Taccio Yamamoto <tyamamo2@illinois.edu>
2018-12-02 03:58:53 -0600

    added dataset

---
+++ dataset.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+import pandas as pd
+import util
+import torch.utils.data
+import os
+
+class ImageDataset(torch.utils.data.Dataset):
+    def __init__(self, base_dir, img_type, transform=None):
+        super(ImageDataset, self).__init__()
+        self.base_dir = base_dir
+        phenotype_filename = os.path.join(base_dir, base_dir.replace('/', '') + '_phenotypic.csv')
+        self.phenotype_info = pd.read_csv(phenotype_filename)
+        self.img_type = img_type
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        info = self.phenotype_info.iloc[idx]
+        dir_name = os.path.join(self.base_dir, info[0])
+        img_name = util.get_img_name(subject_id=info[0], img_type=self.img_type)
+        img = util.open_nii_img(os.path.join(dir_name, img_name))
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, info['DX']
+
+    def __len__(self):
+        return len(self.phenotype_info)
+
+
+def get_data_loader(base_dir, img_type, transform=None, shuffle=True):
+    dataset = ImageDataset(base_dir, img_type, transform=transform)
+    return torch.utils.data.dataloader.DataLoader(dataset, num_workers=32, shuffle=shuffle)
---
+++ util.py
@@ -0,0 +1,46 @@
+import os
+import nibabel as nib
+import numpy as np
+from enum import Enum
+
+DATA_DIR = 'data'
+NYU_DIR_NAME = os.path.join(DATA_DIR, 'NYU')
+
+
+def open_nii_img(filename):
+    return nib.load(filename).get_data()
+
+
+def get_img_name(subject_id, img_type):
+    img_name = None
+    if img_type == ImgType.STRUCTURAL_T1:
+        img_name = STRUCTURAL_T1_FILE_FORMAT.format(subject=subject_id)
+    elif img_type == ImgType.STRUCTURAL_GM:
+        img_name = STRUCTURAL_GM_FILE_FORMAT.format(subject=subject_id)
+    elif img_type == ImgType.STRUCTURAL_FILTER:
+        img_name = STRUCTURAL_FILTER_FILE_FORMAT.format(subject=subject_id)
+    elif img_type == ImgType.STRUCTURAL_TRANSFORM:
+        img_name = STRUCTURAL_TRANSFORM_FILE_FORMAT.format(subject=subject_id)
+    elif img_type == ImgType.FUNCTIONAL_BLURRED:
+        img_name = FUNCTIONAL_BLURRED_FILE_FORMAT.format(subject=subject_id)
+    elif img_type == ImgType.FUNCTIONAL_BANDPASS:
+        img_name = FUNCTIONAL_BANDPASS_FILE_FORMAT.format(subject=subject_id)
+    return img_name
+
+
+class ImgType(Enum):
+    STRUCTURAL_T1 = 1
+    STRUCTURAL_GM = 2
+    STRUCTURAL_FILTER = 3
+    STRUCTURAL_TRANSFORM = 4
+    FUNCTIONAL_BLURRED = 5
+    FUNCTIONAL_BANDPASS = 6
+
+
+STRUCTURAL_T1_FILE_FORMAT = 'wssd{subject}_session_1_anat.nii.gz'
+STRUCTURAL_GM_FILE_FORMAT = 'wssd{subject}_session_1_anat_gm.nii.gz'
+STRUCTURAL_FILTER_FILE_FORMAT = 'swssd{subject}_session_1_anat_gm.nii.gz'
+STRUCTURAL_TRANSFORM_FILE_FORMAT = '{subject}_session_1_template.nii.gz'
+FUNCTIONAL_BLURRED_FILE_FORMAT = 'snwmrda{subject}_session_1_rest_1.nii.gz'
+FUNCTIONAL_BANDPASS_FILE_FORMAT = 'sfnwmrda{subject}_session_1_rest_1.nii.gz'
+# https://github.com/tkipf/pygcn for ReHO data processing
