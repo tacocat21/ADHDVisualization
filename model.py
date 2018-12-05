17b979755d1ffefc4d01c82572d517ec8d3ea409
Taccio Yamamoto <tyamamo2@illinois.edu>
2018-12-05 15:22:20 -0600

    fixed conv order in model.py

--- model.py
+++ model.py
@@ -10,8 +10,8 @@ def max_pool_output_shape(input_shape, max_poolsize):
         res.append(i-(p-1))
     return tuple(res)
 
-def _conv_layer(input_channels, output_channels, kernel_size):
-    return torch.nn.Sequential(nn.Conv3d(input_channels, output_channels, kernel_size=kernel_size),
+def _conv_layer(input_channels, output_channels, kernel_size, padding=1):
+    return torch.nn.Sequential(nn.Conv3d(input_channels, output_channels, kernel_size=kernel_size, padding=padding),
                                nn.ReLU())
 
 # training a 3D conv net on the structural dataset
@@ -22,10 +22,10 @@ class StructuralModel3D(nn.modules.Module):
         self.max_pool_1 = torch.nn.MaxPool3d(max_pool_shape)
         # img_shape = (input_length, *IMAGE_SHAPE)
         # max_pool_output = max_pool_output_shape(img_shape, max_pool_shape)
-        self.conv1 = _conv_layer(1, 32, kernel_size=5)
+        self.conv1 = _conv_layer(1, 32, kernel_size=5, padding=2)
         self.max_pool_2 = torch.nn.MaxPool3d(max_pool_shape)
-        self.conv2 = _conv_layer(32, 32, kernel_size=(3, 5, 5))
-        self.conv3 = _conv_layer(32, 64, kernel_size=(3,3,3))
+        self.conv2 = _conv_layer(32, 32, kernel_size=(3, 5, 5), padding=1)
+        self.conv3 = _conv_layer(32, 64, kernel_size=3)
         self.conv4 = _conv_layer(64, 64, kernel_size=3)
         self.fc = torch.nn.Linear(64, 4)
 
